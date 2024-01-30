055f95c0c743b6e8448b76854c2a55b3e35c2403
JuanFeliciano <139025070+JuanFeliciano@users.noreply.github.com>
2024-01-30 11:23:11 -0300

    feat: sum function added to the code

---
+++ prisma/migrations/20240130135641_calculator/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "ValorOne" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "numberone" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "ValorTwo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "numbertwo" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Resultado" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "resultado" INTEGER NOT NULL
+);
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,47 @@
 import { error } from "console";
 import fastify from "fastify";
+import { prisma } from "./prisma";
 
 const app = fastify();
+
+app.post("/sum", async (req, res) => {
+  try {
+    const { numberone } = (await req.body) as {
+      numberone: number;
+    };
+    const { numbertwo } = (await req.body) as {
+      numbertwo: number;
+    };
+
+    await prisma.resultado.create({
+      data: {
+        resultado: numberone + numbertwo,
+      },
+    });
+
+    const result = await prisma.resultado.findFirst({
+      orderBy: {
+        id: "desc",
+      },
+    });
+
+    await prisma.valorOne.create({
+      data: {
+        numberone,
+      },
+    });
+    await prisma.valorTwo.create({
+      data: {
+        numbertwo,
+      },
+    });
+
+    return res.status(200).send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 app
   .listen({
