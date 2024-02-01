56394f93a406a972cb1b842a52e428afc6b0a06d
JuanFeliciano <139025070+JuanFeliciano@users.noreply.github.com>
2024-02-01 11:01:55 -0300

    feat:all function added to the code

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { prisma } from "./prisma";
 
 const app = fastify();
 async function UserRoutes(app: FastifyInstance) {
+  /* Função de Soma */
   app.post("/sum", async (req, res) => {
     try {
       const { numberone } = (await req.body) as {
@@ -15,6 +16,123 @@ async function UserRoutes(app: FastifyInstance) {
       await prisma.resultado.create({
         data: {
           resultado: numberone + numbertwo,
+        },
+      });
+
+      const result = await prisma.resultado.findFirst({
+        orderBy: {
+          id: "desc",
+        },
+      });
+
+      await prisma.valorOne.create({
+        data: {
+          numberone,
+        },
+      });
+      await prisma.valorTwo.create({
+        data: {
+          numbertwo,
+        },
+      });
+
+      return res.status(200).send(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+  app.post("/sub", async (req, res) => {
+    try {
+      const { numberone } = (await req.body) as {
+        numberone: number;
+      };
+      const { numbertwo } = (await req.body) as {
+        numbertwo: number;
+      };
+
+      await prisma.resultado.create({
+        data: {
+          resultado: numberone - numbertwo,
+        },
+      });
+
+      const result = await prisma.resultado.findFirst({
+        orderBy: {
+          id: "desc",
+        },
+      });
+
+      await prisma.valorOne.create({
+        data: {
+          numberone,
+        },
+      });
+      await prisma.valorTwo.create({
+        data: {
+          numbertwo,
+        },
+      });
+
+      return res.status(200).send(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+  app.post("/mult", async (req, res) => {
+    try {
+      const { numberone } = (await req.body) as {
+        numberone: number;
+      };
+      const { numbertwo } = (await req.body) as {
+        numbertwo: number;
+      };
+
+      await prisma.resultado.create({
+        data: {
+          resultado: numberone * numbertwo,
+        },
+      });
+
+      const result = await prisma.resultado.findFirst({
+        orderBy: {
+          id: "desc",
+        },
+      });
+
+      await prisma.valorOne.create({
+        data: {
+          numberone,
+        },
+      });
+      await prisma.valorTwo.create({
+        data: {
+          numbertwo,
+        },
+      });
+
+      return res.status(200).send(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+  app.post("/div", async (req, res) => {
+    try {
+      const { numberone } = (await req.body) as {
+        numberone: number;
+      };
+      const { numbertwo } = (await req.body) as {
+        numbertwo: number;
+      };
+
+      await prisma.resultado.create({
+        data: {
+          resultado: numberone / numbertwo,
         },
       });
 
